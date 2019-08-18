66691a0b96d1b76d8f4cdf62d73c917d385b5bdd
Sentinels23 <sentinel1992@mail.ru>
2019-08-18 21:39:11 +0300

    Реализована реакция кнопок CurrencySwitcher на собственные действия
    Добавлен биндинг для поля set_selected currency_switcher`а (его бэкграунда)
    к полю btcSelected
    ICurrencySwitcherViewModel теперь содержит btcSelected поле и btc/ltcSelect
    функции

--- app/src/main/java/com/sigmapool/app/screens/poolInfo/viewmodel/ICurrencySwitcherViewModel.kt
+++ app/src/main/java/com/sigmapool/app/screens/poolInfo/viewmodel/ICurrencySwitcherViewModel.kt
@@ -1,5 +1,10 @@
 package com.sigmapool.app.screens.poolInfo.viewmodel
 
+import androidx.databinding.ObservableBoolean
+
 interface ICurrencySwitcherViewModel {
-    fun setCurrencySelected(isSelected: Boolean)
+    val btcSelected: ObservableBoolean
+    fun btcSelect()
+    fun ltcSelect()
+    //fun setCurrencySelected(isSelected: Boolean)
 }
