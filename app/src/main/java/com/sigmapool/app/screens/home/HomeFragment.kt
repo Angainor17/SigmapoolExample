2d1133c358bd09f2e78f91e22d9b51ed95059131
angai <angainor17@gmail.com>
2019-08-22 15:23:40 +0300

    home fragment toolbar

--- app/src/main/java/com/sigmapool/app/screens/home/HomeFragment.kt
+++ app/src/main/java/com/sigmapool/app/screens/home/HomeFragment.kt
@@ -1,21 +1,33 @@
 package com.sigmapool.app.screens.home
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.sigmapool.app.databinding.FragmentHomeBinding
 import com.sigmapool.app.screens.home.viewModel.HomeVM
+
 
 class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = FragmentHomeBinding.inflate(inflater, container, false)
+
         val vm = HomeVM()
+        vm.urlLiveData.observe(this, Observer { openUrlInBrowser(it) })
+
         binding.vm = vm
         binding.lifecycleOwner = this
 
         return binding.root
+    }
+
+    private fun openUrlInBrowser(url: String) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
     }
 }
--- app/src/main/java/com/sigmapool/app/screens/home/viewModel/HomeVM.kt
+++ app/src/main/java/com/sigmapool/app/screens/home/viewModel/HomeVM.kt
@@ -1,9 +1,19 @@
 package com.sigmapool.app.screens.home.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+
+private const val REGISTER_URL = "https://btc.sigmapool.com/signup"
 
 class HomeVM : ViewModel() {
 
+    val urlLiveData = MutableLiveData<String>()
 
+    fun register() {
+        urlLiveData.postValue(REGISTER_URL)
+    }
 
+    fun login() {
+        //TODO implement
+    }
 }
