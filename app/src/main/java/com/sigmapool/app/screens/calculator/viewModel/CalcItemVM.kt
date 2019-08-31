c6890f3786a928e524c799bd234b04d30f4ae3c7
angai <angainor17@gmail.com>
2019-08-31 12:27:30 +0300

    converter view

--- app/src/main/java/com/sigmapool/app/screens/calculator/viewModel/CalcItemVM.kt
+++ app/src/main/java/com/sigmapool/app/screens/calculator/viewModel/CalcItemVM.kt
@@ -1,7 +1,17 @@
 package com.sigmapool.app.screens.calculator.viewModel
 
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CalcItemVM(
     val coinLabel: String
-) : ViewModel()
+) : ViewModel() {
+
+    val price = MutableLiveData("~ \$ 368.48")//FIXME
+
+    val clickAction = View.OnClickListener {
+        Log.d("", "")
+    }
+}
