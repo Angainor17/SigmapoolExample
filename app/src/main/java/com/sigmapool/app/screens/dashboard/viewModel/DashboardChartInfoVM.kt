be705604e25c697d9f0034b07e15d0d5ac3ed06c
angai <angainor17@gmail.com>
2019-09-14 21:18:30 +0300

    switcher

--- app/src/main/java/com/sigmapool/app/screens/dashboard/viewModel/DashboardChartInfoVM.kt
+++ app/src/main/java/com/sigmapool/app/screens/dashboard/viewModel/DashboardChartInfoVM.kt
@@ -2,11 +2,23 @@ package com.sigmapool.app.screens.dashboard.viewModel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sigmapool.app.App.Companion.kodein
+import com.sigmapool.app.R
 import com.sigmapool.app.provider.coin.ICoinProvider
+import com.sigmapool.app.provider.res.IResProvider
+import com.sigmapool.app.utils.customViews.CustomSwitchVm
+import org.kodein.di.generic.instance
 
 class DashboardChartInfoVM(
     private val coinProvider: ICoinProvider
 ) : ViewModel() {
+
+    private val resProvider by kodein.instance<IResProvider>()
+
+    val customSwitchVm = CustomSwitchVm(
+        resProvider.getString(R.string.one_hour),
+        resProvider.getString(R.string.one_day)
+    )
 
     val coin = MutableLiveData<String>(coinProvider.getLabel().toUpperCase())
 
--- app/src/main/java/com/sigmapool/app/utils/customViews/CustomSwitchVm.kt
+++ app/src/main/java/com/sigmapool/app/utils/customViews/CustomSwitchVm.kt
@@ -1,9 +1,27 @@
 package com.sigmapool.app.utils.customViews
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class CustomSwitchVm : ViewModel() {
+class CustomSwitchVm(
+    val leftText: String = "",
+    val rightText: String = ""
+) : ViewModel() {
 
+    val leftActivated = MutableLiveData(false)
+    val rightActivated = MutableLiveData(true)
 
+    fun rightClick() {
+        if (!rightActivated.value!!) {
+            leftActivated.postValue(false)
+            rightActivated.postValue(true)
+        }
+    }
 
+    fun leftClick() {
+        if (!leftActivated.value!!) {
+            rightActivated.postValue(false)
+            leftActivated.postValue(true)
+        }
+    }
 }
