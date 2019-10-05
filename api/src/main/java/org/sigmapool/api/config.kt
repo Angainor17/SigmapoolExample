b283d18f5c424caf155754c01d128f2b40541c7e
angai <angainor17@gmail.com>
2019-10-05 20:22:22 +0300

    Release

--- api/src/main/java/org/sigmapool/api/config.kt
+++ api/src/main/java/org/sigmapool/api/config.kt
@@ -4,6 +4,6 @@ package org.sigmapool.api
 private const val TEST_BASE_URL = "proofpool.com"
 private const val PROD_BASE_URL = "sigmapool.com"
 
-private const val isTestMode = true
+private val isTestMode = false
 
 val BASE_URL = if (isTestMode) TEST_BASE_URL else PROD_BASE_URL
